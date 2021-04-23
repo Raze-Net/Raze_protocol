87f1f4c3ff3f12ca96245291e3454ab6d05ce053
resovepant8 <resovepant8@gmail.com>
2021-04-23 17:17:04 +0800

    update deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -15,18 +15,30 @@ module.exports = function(deployer) {
         deployer.deploy(RazeIP)
     ])
     .then(() => {
-        console.log("Deploying RazeRedeem, RazeTransfer...");
+        console.log("Deploying RazeRedeem...");
         return Promise.all([
             deployer.deploy(RazeRedeem, RazeIP.address),
+        ]);
+    })
+    .then(() => {
+        console.log("Deploying RazeTransfer...");
+        return Promise.all([
             deployer.deploy(RazeTransfer, RazeIP.address)
         ]);
     })
     .then(() => {
-        console.log("Deploying RazeETH, RazeERC20...");
+        console.log("Deploying RazeETH...");
         return Promise.all([
             // Should use string for large number. This seems to be a bug:
             // https://github.com/ethereum/web3.js/issues/2077
             deployer.deploy(RazeETH, RazeTransfer.address, RazeRedeem.address, "10000000000000000"),
+        ]);
+    })
+    .then(() => {
+        console.log("Deploying RazeERC20...");
+        return Promise.all([
+            // Should use string for large number. This seems to be a bug:
+            // https://github.com/ethereum/web3.js/issues/2077
             deployer.deploy(RazeERC20, TestRazeToken.address, RazeTransfer.address, RazeRedeem.address, "10000000000000000000000")
         ]);
     });
